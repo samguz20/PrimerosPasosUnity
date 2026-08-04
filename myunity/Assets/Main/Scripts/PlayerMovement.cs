bb6c1625a4c37d790f81285367f70681b3e9d4ba
Samuel Guzmán <samuelguzmangomez2@gmail.com>
2026-08-04 11:28:43 -0500

    Variables 4ta clase

--- myunity/Assets/Main/Scripts/PlayerMovement.cs
+++ myunity/Assets/Main/Scripts/PlayerMovement.cs
@@ -5,25 +5,32 @@ public class PlayerMovement : MonoBehaviour
 
     //Variables
     [SerializeField]
-   private int _edad = 17;
-   private float _velocidad = 15.5f;
-   public string nombre = "Samuel";
-   public bool isJumping = false;
-
-    public float fuerza = 500f;
-
-    [SerializeField] private Rigidbody2D _rigibody2D;
+    public float altura = 1.82f;
+    public int edad = 17;
+    public string nombre = "Samuel";
+    public bool puedeVotar = true;
 
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public GameObject gameObject;
+    public Rigidbody2D rigidbody2D;
+    public Collider2D collider2D;
+    public SpriteRenderer spriteRenderer;
+    public Transform transform;
+    //crear una variable de tipo transform
+
+    private void Start()
     {
-        _rigibody2D.AddForce(Vector2.right * fuerza);
+        Debug.Log("Hola: " + nombre + "tu edad es: " + edad + "tu altura es: " + altura);
+        rigidbody2D.simulated = false;
+        spriteRenderer.color = Color.yellow;
+        //llamo mi variable de tipo traansform y le asigno un valor en x de 10
+        transform.position = new Vector3(5f, 0f, 0f);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        Debug.Log("Update");
     }
 }
+
+
+     
