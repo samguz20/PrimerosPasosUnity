3111d87afc31b2bd3a18fc487f1cc4fa6348deb3
samguz20 <samuelguzmangomez2@gmail.com>
2026-08-06 11:41:18 -0500

    If jump - 5ta clase

--- myunity/Assets/Main/Scripts/PlayerMovement.cs
+++ myunity/Assets/Main/Scripts/PlayerMovement.cs
@@ -2,35 +2,22 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-
-    //Variables
-    [SerializeField]
-    public float altura = 1.82f;
-    public int edad = 17;
-    public string nombre = "Samuel";
-    public bool puedeVotar = true;
-
-
-    public GameObject gameObject;
-    public Rigidbody2D rigidbody2D;
-    public Collider2D collider2D;
-    public SpriteRenderer spriteRenderer;
-    public Transform transform;
-    //crear una variable de tipo transform
-
+    [SerializeField] private float _force = 5f;
+    [SerializeField] private float _speed = 5f;
+    [SerializeField] private Rigidbody2D _rigidbody2D;
     private void Start()
     {
-        Debug.Log("Hola: " + nombre + "tu edad es: " + edad + "tu altura es: " + altura);
-        rigidbody2D.simulated = false;
-        spriteRenderer.color = Color.yellow;
-        //llamo mi variable de tipo traansform y le asigno un valor en x de 10
-        transform.position = new Vector3(5f, 0f, 0f);
-    }
+        _rigidbody2D = GetComponent<Rigidbody2D>();
 
+    }
     private void Update()
     {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            _rigidbody2D.AddForce(Vector2.up * _force);
+        }
+        _rigidbody2D.velocity = Vector2.right * _speed * Time.deltaTime;
     }
-}
 
 
-     
+}   
