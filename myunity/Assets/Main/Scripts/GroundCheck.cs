39553be946ffde8fce32785200b22ac07cf65f68
samguz20 <samuelguzmangomez2@gmail.com>
2026-08-20 11:43:44 -0500

    Terminación del juego
    Entrega parcial del juego

--- myunity/Assets/Main/Scripts/GroundCheck.cs
+++ myunity/Assets/Main/Scripts/GroundCheck.cs
@@ -3,13 +3,14 @@ using UnityEngine;
 public class GroundCheck : MonoBehaviour
 {
     public bool isGround;
-    private void OnTriggerEnter2D(Collider2D colission)
+    private void OnTriggerStay2D(Collider2D colission)
     {
        isGround = true;
         Debug.Log("Me tocaron!!");
     }
     private void OnTriggerExit2D(Collider2D colission)
     {
+        isGround = false;
         Debug.Log("Me dejaron de tocar");
     }
 }
