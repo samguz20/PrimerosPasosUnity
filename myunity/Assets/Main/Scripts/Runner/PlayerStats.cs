9c573568c03374ba664c76d62b39ad6907ce0cb6
samguz20 <samuelguzmangomez2@gmail.com>
2026-09-03 11:38:31 -0500

    Curacion y daño

--- myunity/Assets/Main/Scripts/Runner/PlayerStats.cs
+++ myunity/Assets/Main/Scripts/Runner/PlayerStats.cs
@@ -2,26 +2,18 @@ using UnityEngine;
 
 public class PlayerStats : MonoBehaviour
 {
-    [SerializeField] private float _puntosVida = 100;
+    [SerializeField] private int _puntosVida = 100;
+    private int _sumarVida = 10;
     public void RestarVida(int daño)
     {
         _puntosVida = _puntosVida - daño;
+        
 
-        // + suma
-        // - resta
-        // / division
-        // * multiplicacion
+    }
+    public void SumarVida(int heal)
+    {
+        _puntosVida = _puntosVida + heal;
 
-        // = asignar
-        // == comparar
-        // ! diferente
-        // && y
-        // || o
 
-        // !=
-        // -=
-        // +=
-        // ++ sumar 1
-        // -- restar 1
     }
 }
