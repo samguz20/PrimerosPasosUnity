4232179d140d333dc922ef0f3849acdd4cfcad88
Tomas <tommy28gomez@gmail.com>
2026-09-15 11:42:11 -0500

    ajuste vida y colores

--- myunity/Assets/Main/Scripts/UI/UIManager.cs
+++ myunity/Assets/Main/Scripts/UI/UIManager.cs
@@ -4,15 +4,23 @@ public class UIManager : MonoBehaviour
 
 {
     [SerializeField] private Image _barra;
-    [SerializeField] private UIManager _uiManager;
+
 
     public void SumarFillAmount(float amount)
     {
         _barra.fillAmount += amount;
-        _barra.fillAmount= amount + _barra.fillAmount;
+
     }
 
     public void RestarFillAmount(float amount)
     {
         _barra.fillAmount = _barra.fillAmount - amount;
     }
+
+    public void ColorBarra(Color myColor)
+    {
+        _barra.color = myColor;
+
+
+    }
+}
--- myunity/Assets/Main/Scripts/runner/PlayerStats.cs
+++ myunity/Assets/Main/Scripts/runner/PlayerStats.cs
@@ -1,40 +1,51 @@
-using System.Runtime.CompilerServices;
-using TMPro.EditorUtilities;
+
 using UnityEngine;
 
 public class PlayerStats : MonoBehaviour
 {
-    [SerializeField] private float _puntosVida = 100f;
+    [SerializeField] private int _puntosVidaActuales = 100;
+    [SerializeField] private int _puntosVidaMaximo = 100;
     [SerializeField] private UIManager _uiManager;
     public void RestarVida(int daño)
     {
-        _puntosVida = _puntosVida - daño;
-        _puntosVida -= daño;
+        _puntosVidaActuales = _puntosVidaActuales - daño;
+        
     }
 
 
-    public void SumarVida(int Curacion)
+    public void SumarVida(int vida)
     {
-        _puntosVida = _puntosVida + Curacion;
-      _puntosVida += Curacion;
-     }
-
-}
-  private void Update()
-{
-    if (/*vida del jugador*/ >= 80)
-    {
-        _uiManager.color = (Color.white);
+     _puntosVidaActuales += vida;
     }
-
-    if (40 <= /*vida del jugador*/ < 80)
+    private void Update()
     {
-        _uiManager.ColorBarra = (new Color(1f, 1f, 1f, 1f));
+        if (_puntosVidaActuales >= 80)
+        {
+            _uiManager.ColorBarra(new Color(68f, 189f, 68f, 255f));
+        }
+
+        if (40 <= _puntosVidaActuales && _puntosVidaActuales < 80)
+        {
+            _uiManager.ColorBarra(new Color(245f, 73f, 39, 255));
+        }
+
+        if (_puntosVidaActuales < 40)
+        {
+            _uiManager.ColorBarra(new Color(189f, 15f, 255f));
+        }
+
+
+        if (_puntosVidaActuales >100)   
+        {
+            _puntosVidaActuales = 100;
+        }
+        if (_puntosVidaActuales <= 0)
+        {
+            Destroy(this.gameObject);
+        }
+
+        {
+            Debug.Log("Game Over");
+        }
     }
-
-    if (/*vida del jugador*/ < 40)
-    {
-        _uiManager.ColorBarra = (new Color(1f, 0f, 0f, 1f));
-    } 
-
 }
